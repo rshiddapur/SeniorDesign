9ae6ede4f5a6295e49aba7727dea88e70ccbc07d
rshiddapur <rshiddapur@gmail.com>
2019-09-18 16:21:30 -0400

    Major refactor - added global hardware definitions to setup() method and removed from initialize() method

--- main/initialize.ino
+++ main/initialize.ino
@@ -3,53 +3,8 @@
  * the robot is working.
  */
 
-// Library for controlling the hobby servo
-#include <Servo.h>
-
-
-// Motor driver pin connections
-#define ENB 5  // left motor enable
-#define IN1 7  // left motor control
-#define IN2 8  // left motor control
-#define ENA 6  // right motor enable
-#define IN3 9  // right motor control
-#define IN4 11 // right motor control
-
-// Ultrasonic rangefinder pin connections
-#define Echo A4
-#define Trig A5
-
-// Servo pins
-#define ServoPin 3
-
-// Macros to read the three line tracking sensors.
-// These will return '0' with a light surface and '1'
-// with a dark surface.
-#define LT_R !digitalRead(10)
-#define LT_M !digitalRead(4)
-#define LT_L !digitalRead(2)
-
-// global variables
-Servo head; // create servo object to control the looking direction
-
      void initialize()
     {
-        // Start serial comm in case you want to debug with it
-        Serial.begin(9600);
-
-        // Configure the pins that are outputs
-        pinMode(Trig, OUTPUT);
-        pinMode(IN1, OUTPUT);
-        pinMode(IN2, OUTPUT);
-        pinMode(IN3, OUTPUT);
-        pinMode(IN4, OUTPUT);
-        pinMode(ENA, OUTPUT);
-        pinMode(ENB, OUTPUT);
-        pinMode(ServoPin, OUTPUT);
-
-        // Attach the servo controller to the servo pin
-        head.attach(ServoPin);
-
         // Set the head looking to the side.
         head.write(0);
         delay(2000);
--- main/station.ino
+++ main/station.ino
@@ -1,13 +1,3 @@
-// Library for controlling the hobby servo
-#include <Servo.h>
-// Ultrasonic rangefinder pin connections
-#define Echo A4
-#define Trig A5
-
-// Servo pins
-#define ServoPin 3
-
-
 void station() {
     //delay (2000);
     servoTest();
@@ -19,11 +9,6 @@ void station() {
 
 
 void servoTest() {
-    pinMode(ServoPin, OUTPUT);
-
-    // Attach the servo controller to the servo pin
-    head.attach(ServoPin);
-
     // Set the head looking to the side.
     head.write(0);
     delay(2000);
